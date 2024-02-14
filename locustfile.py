d9af1fe891fae7143a9dde0c71a8cb692fe30b25
Saurabh Bhardwaj <aryan.saurabhbhardwaj@gmail.com>
2024-02-14 16:50:50 +0000

    adding images for readme

--- locustfile.py
+++ locustfile.py
@@ -1,32 +1,13 @@
+import time
 from locust import HttpUser, task, between
 
-class WebsiteTestUser(HttpUser):
-    wait_time = between(0.5, 3.0)
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 2.5)
 
-    @task(1)
-    def test_root(self):
-        self.client.get("")
+    @task
+    def hello_world(self):
+        self.client.get("/")
 
-    @task(2)
-    def test_predict(self):
-        self.client.post("predict", json={
-                                "CHAS":{
-                                    "0":0
-                                },
-                                "RM":{
-                                    "0":6.575
-                                },
-                                "TAX":{
-                                    "0":296.0
-                                },
-                                "PTRATIO":{
-                                    "0":15.3
-                                },
-                                "B":{
-                                    "0":396.9
-                                },
-                                "LSTAT":{
-                                    "0":4.98
-                                }
-                            }
-                        )
+    def on_start(self):
+        self.client.post("/predict", json={"username":"foo", "password":"bar"})
+        host = 'pricehouseprediction.azurewebsites.net'
